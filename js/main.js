3af972b67f088d853eacc9a65cebc68df6ea3ce2
buryk20 <73736907+buryk20@users.noreply.github.com>
2021-09-05 16:01:44 +0300

    mob

--- js/main.js
+++ js/main.js
@@ -1,41 +1,88 @@
-function checkFluency(){
-var checkbox = document.getElementById('InverterEfficiencyPromt');
-var InverterEfficiency = document.getElementById('InverterEfficiency');
-var InverterEfficiencyColor = document.getElementById('InverterEfficiencyColor');
-        if (checkbox.checked != true){
-            InverterEfficiency.setAttribute('disabled', 'disabled');
-            InverterEfficiency.style.borderColor = "#F1F1F1";
-            InverterEfficiencyColor.style.color = "#8A8A8A";
-            if(InverterEfficiency.value != null) {
-                InverterEfficiency.value = '0.8';
-                InverterEfficiency.setAttribute('placeholder', '180');
-            }
-    } else {
-        if(InverterEfficiency.value != null) {
-            InverterEfficiency.value = ' ';
-        }
-        InverterEfficiency.removeAttribute('disabled');
-        InverterEfficiency.style.borderColor = "#8A8A8A";
-        InverterEfficiency.removeAttribute('placeholder');
-        InverterEfficiencyColor.style.color = "#000000";
+function checkFluency() {
+  var checkbox = document.getElementById('InverterEfficiencyPromt');
+  var InverterEfficiency = document.getElementById('InverterEfficiency');
+  var InverterEfficiencyColor = document.getElementById('InverterEfficiencyColor');
+  if (checkbox.checked != true) {
+    InverterEfficiency.setAttribute('disabled', 'disabled');
+    InverterEfficiency.style.borderColor = "#F1F1F1";
+    InverterEfficiencyColor.style.color = "#8A8A8A";
+    if (InverterEfficiency.value != null) {
+      InverterEfficiency.value = '0.8';
+      InverterEfficiency.setAttribute('placeholder', '180');
     }
   }
-    
-  
+  else {
+    if (InverterEfficiency.value != null) {
+      InverterEfficiency.value = '';
+    }
+    InverterEfficiency.removeAttribute('disabled');
+    InverterEfficiency.style.borderColor = "#8A8A8A";
+    InverterEfficiency.removeAttribute('placeholder');
+    InverterEfficiencyColor.style.color = "#000000";
+  }
+}
+function checkFluencyAkb() {
+  var checkboxAkb = document.getElementById('InverterEfficiencyPromtAkb');
+  var InverterEfficiencyAkb = document.getElementById('InverterEfficiencyAkb');
+  var InverterEfficiencyColorAkb = document.getElementById('InverterEfficiencyColorAkb');
+  if (checkboxAkb.checked != true) {
+    InverterEfficiencyAkb.setAttribute('disabled', 'disabled');
+    InverterEfficiencyAkb.style.borderColor = "#F1F1F1";
+    InverterEfficiencyColorAkb.style.color = "#8A8A8A";
+    if (InverterEfficiencyAkb.value != null) {
+      InverterEfficiencyAkb.value = '0.8';
+      InverterEfficiencyAkb.setAttribute('placeholder', '180');
+    }
+  } else {
+    if (InverterEfficiencyAkb.value != null) {
+      InverterEfficiencyAkb.value = ' ';
+    }
+    InverterEfficiencyAkb.removeAttribute('disabled');
+    InverterEfficiencyAkb.style.borderColor = "#8A8A8A";
+    InverterEfficiencyAkb.removeAttribute('placeholder');
+    InverterEfficiencyColorAkb.style.color = "#000000";
+  }
+}
 
 
 
-  function calculation () {
-    var time = Number(document.getElementById('time').value);
-    var v  = Number(document.getElementById('v').value);
-    var efficiency = Number(InverterEfficiency.value);
-    var w = Number(document.getElementById('w').value);
-    var results = document.getElementById('results');
-    
-    
-    results.value = ((time*v)/efficiency)/w;
-     
-    // console.log(a);
+
+function calculation() {
+  var time = Number(document.getElementById('time').value);
+  var v = Number(document.getElementById('v').value);
+  var efficiency = Number(InverterEfficiency.value);
+  var w = Number(document.getElementById('w').value);
+  var results = document.getElementById('results');
+  if (w == 0) {
+    alert('Введите нагрузку')
+  } else if (efficiency == 0) {
+    alert('Введите значение КПД инвертора, или переключите в заначение по умолчанию 0,8')
+  } else if (time == 0) {
+    alert('Введите значение время')
+  } else if (v == 0) {
+    alert('Введите напряжение')
+  } else {
+    results.value = (((time * v) / efficiency) / w).toFixed(2);
+    console.log(results.value);
+  }
+}
+
+function calculationAkb() {
+  var wAkb = Number(document.getElementById('wAkb').value);
+  var efficiencyAkb = InverterEfficiencyAkb.value;
+  var ahAkb = Number(document.getElementById('ahAkb').value);
+  var vAkb = Number(document.getElementById('vAkb').value);
+  var resultsAkb = document.getElementById('resultsAkb');
+  if (wAkb == 0) {
+    alert('Введите нагрузку')
+  } else if (efficiencyAkb == 0) {
+    alert('Введите значение КПД инвертора, или переключите в заначение по умолчанию 0,8')
+  } else if (ahAkb == 0) {
+    alert('Введите емкость АКБ')
+  } else if (vAkb == 0) {
+    alert('Введите напряжение')
+  } else {
+    resultsAkb.value = (((ahAkb * wAkb) / vAkb) * efficiencyAkb).toFixed(2);
   }
 
-//   console.log(a);
+}
