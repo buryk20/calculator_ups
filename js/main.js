8210ea1e00a70a78fe26f54b08fd9409354c8737
buryk20 <73736907+buryk20@users.noreply.github.com>
2021-09-09 09:33:10 +0300

    Andrey

--- js/main.js
+++ js/main.js
@@ -3,9 +3,11 @@ function checkFluency() {
   var InverterEfficiency = document.getElementById('InverterEfficiency');
   var InverterEfficiencyColor = document.getElementById('InverterEfficiencyColor');
   if (checkbox.checked != true) {
+    // Выключеный чек бокс
     InverterEfficiency.setAttribute('disabled', 'disabled');
     InverterEfficiency.style.borderColor = "#F1F1F1";
     InverterEfficiencyColor.style.color = "#8A8A8A";
+    InverterEfficiency.style.color = "#8a8a8a";
     if (InverterEfficiency.value != null) {
       InverterEfficiency.value = '0.8';
       InverterEfficiency.setAttribute('placeholder', '180');
@@ -19,6 +21,7 @@ function checkFluency() {
     InverterEfficiency.style.borderColor = "#8A8A8A";
     InverterEfficiency.removeAttribute('placeholder');
     InverterEfficiencyColor.style.color = "#000000";
+    InverterEfficiency.style.color = "#000000";
   }
 }
 function checkFluencyAkb() {
@@ -29,6 +32,7 @@ function checkFluencyAkb() {
     InverterEfficiencyAkb.setAttribute('disabled', 'disabled');
     InverterEfficiencyAkb.style.borderColor = "#F1F1F1";
     InverterEfficiencyColorAkb.style.color = "#8A8A8A";
+    InverterEfficiencyAkb.style.color = "#8a8a8a";
     if (InverterEfficiencyAkb.value != null) {
       InverterEfficiencyAkb.value = '0.8';
       InverterEfficiencyAkb.setAttribute('placeholder', '180');
@@ -41,10 +45,9 @@ function checkFluencyAkb() {
     InverterEfficiencyAkb.style.borderColor = "#8A8A8A";
     InverterEfficiencyAkb.removeAttribute('placeholder');
     InverterEfficiencyColorAkb.style.color = "#000000";
+    InverterEfficiencyAkb.style.color = "#000000";
   }
 }
-
-
 function calculation() {
   var time = Number(document.getElementById('time').value);
   var v = Number(document.getElementById('v').value);
@@ -59,9 +62,9 @@ function calculation() {
   } else if (efficiency == 0) {
     alert('Введите значение КПД инвертора, или переключите в заначение по умолчанию 0,8')
   } else if (time == 0) {
-    alert('Введите значение время')
+    alert('Введите значение Время работы')
   } else if (v == 0) {
-    alert('Введите напряжение')
+    alert('Введите Номинальное напряжение АКБ')
   } else {
     a = w / v;
     Ah = a * time;
@@ -70,18 +73,15 @@ function calculation() {
     } else {
       total = Ah/(efficiency)
     }
-    results.value = total.toFixed(2);
+    results.value = total.toFixed(0);
   }
 }
-
 function calculationAkb() {
   var wAkb = Number(document.getElementById('wAkb').value);
   var efficiencyAkb = InverterEfficiencyAkb.value;
   var ahAkb = Number(document.getElementById('ahAkb').value);
   var vAkb = Number(document.getElementById('vAkb').value);
   var resultsAkb = document.getElementById('resultsAkb');
-  var resultsAkbMin = document.getElementById('resultsAkbMin');
-  var minutes;
   var watch;
   if (wAkb == 0) {
     alert('Введите нагрузку')
@@ -90,19 +90,29 @@ function calculationAkb() {
   } else if (ahAkb == 0) {
     alert('Введите емкость АКБ')
   } else if (vAkb == 0) {
-    alert('Введите напряжение')
+    alert('Введите Номинальное напряжение АКБ')
   } else {
     if (efficiencyAkb > 1){
       watch = (((ahAkb * vAkb ) / wAkb) * (efficiencyAkb/100)).toFixed(2);
     } else {
       watch = (((ahAkb * vAkb ) / wAkb) * (efficiencyAkb)).toFixed(2);
     }
-    resultsAkb.value = watch;
-    minutes = (watch * 60).toFixed(2);
-    resultsAkbMin.value = minutes;
+    console.log(watch);
+    if(watch <= 1) {
+      resultsAkb.value = ` ${Math.ceil(watch*60)}min`;
+    } else if (watch > 1) {
+      var integer;
+      var fraction;
+      var sum;
+      integer = Math.floor(Number(watch));
+      console.log(integer);
+      fraction = (watch - integer).toFixed(2);
+      sum = (fraction*60);
+      console.log(fraction);
+      resultsAkb.value = `${integer}h ${sum}min`;
+    }
   }
 }
-
 var descriptionOpenClosing = document.getElementById('descriptionOpenClosing');
 var arrowAnimation = document.querySelector('.ups-calc-arrow__transform');
 var expandedText = document.querySelector('.ups-calc-text__active');
